e858cc61d9b528b046c263abec646446c9941621
苑 <33371662+Yuandiaodiaodiao@users.noreply.github.com>
2020-11-29 02:43:06 +0800

    打包优化使用electronbuilder正常运行

---
+++ ui/getpath.js
@@ -0,0 +1,11 @@
+const path=require("path")
+function getpath(pathname){
+    console.log(process.cwd())
+    console.log(process.env.NODE_ENV)
+    let templateFilePath = path.join(process.cwd(), '/resources/',pathname)
+    if (process.env.NODE_ENV === 'development') {
+        templateFilePath = path.join(__dirname, pathname)
+    }
+    return templateFilePath
+}
+module.exports=getpath
