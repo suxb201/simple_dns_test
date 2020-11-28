4f5ac25d8469ecb42f1b69d1ff9d5f1631b442cb
苑 <33371662+Yuandiaodiaodiao@users.noreply.github.com>
2020-11-29 00:41:19 +0800

    electron package打包通过

--- ui/subprocess.js
+++ ui/subprocess.js
@@ -1,13 +1,13 @@
 const path = require('path')
-let script = path.join(__dirname, 'pyexe', 'main.exe')
+let script = path.join(__dirname, 'pyexe', "main",'main.exe')
 let scriptconfig = path.join(__dirname, 'pyexe', 'config.toml')
 let pyProc = require('child_process').execFile(script,["-c",scriptconfig])
 // let pyProcx = require('child_process').spawn( "python",[path.join(__dirname, 'text.py'),"-c","aaa"])
 //
-// pyProcx.stdout.on('data', (data) => {
-//     console.log(`stdout: ${data}`);
-// });
-// pyProcx.stderr.on('data', (data) => {
-//     console.error(`stderr: ${data}`);
-// });
+pyProc.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+pyProc.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
 module.exports.proc=pyProc
